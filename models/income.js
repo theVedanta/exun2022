7c35ca20f2e922c66249255f33f8638fa1179756
Vedanta Somnathe <vedanta1412@gmail.com>
2022-01-18 21:55:27 +0530

    css fix and chart

--- models/income.js
+++ models/income.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const IncomeSchema = new Schema({
-    quantity = Number
+    value: Number,
+    history: Array,
 });
 
 const model = mongoose.model("income", IncomeSchema);
